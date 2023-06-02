f5cb5994328d8aaa826fa8a8345645d28d65d989
Evandromck <evandroaraujomck@gmail.com>
2023-06-01 22:13:49 -0300

    feat(implementando o typeorm): adicinando ao Module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,23 @@
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+
+@Module({
+  imports: [
+    TypeOrmModule.forRoot(),
+  ],
+  controllers: [],
+  providers: [],
+})
+export class AppModule {}
+
+
+
 
 @Module({
   imports: [],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule2 {} 
