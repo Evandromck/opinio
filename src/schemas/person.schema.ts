20e80332e65029fbfe1cf2c8181d4296ccc06512
Evandromck <evandroaraujomck@gmail.com>
2023-06-04 23:18:26 -0300

    feat(melhorias swagger doc): define que a propriedade "email" deve ser uma string

--- src/schemas/person.schema.ts
+++ src/schemas/person.schema.ts
@@ -3,27 +3,28 @@ import { ApiProperty } from "@nestjs/swagger";
 
 export class PersonSchema {
   @ApiProperty({
-    description: 'O nome da pessoa',
-    example: 'Evandro Araujo',
+    description: 'Array de perguntas do questionário',
+    example: [{ text: 'Qual é a sua cor favorita?' }],
   })
   @IsString() // Define que a propriedade "name" deve ser uma string
   @MaxLength(120) // Define o tamanho máximo da propriedade "name" como 120 caracteres
   name: string; // Propriedade "name" do tipo string
 
   @ApiProperty({
-    description: 'A idade da pessoa',
-    example: 25,
+    description: 'ID do questionário',
+    example: 1,
   })
   @IsInt() // Define que a propriedade "age" deve ser um número inteiro
   @Min(1) // Define o valor mínimo da propriedade "age" como 1
-  age: number; // Propriedade "age" do tipo number
+  
+  Pergunta: string; // Propriedade "age" do tipo number
 
   @ApiProperty({
-    description: 'O e-mail da pessoa',
-    example: 'Evandro@example.com',
+    description: 'Array de respostas para as perguntas do questionário',
+    example: ['Azul', 'Vermelho', 'Verde'],
   })
   @IsString() // Define que a propriedade "email" deve ser uma string
   @IsEmail() // Define que a propriedade "email" deve ser um endereço de e-mail válido
   @MaxLength(255) // Define o tamanho máximo da propriedade "email" como 255 caracteres
-  email: string; // Propriedade "email" do tipo string
+  Resposta: string; // Propriedade "email" do tipo string
 }
