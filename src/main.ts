d62d8bed394d265caf005484df06a938d5748edc
Evandromck <evandroaraujomck@gmail.com>
2023-06-05 10:56:58 -0300

    feat(finalizando a api): configuração do TypeORM e criação das entidades com MVC
    documentação automática da API usando o Swagger
    
    "fix #8"

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,5 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { ValidationPipe } from '@nestjs/common';
-import { TransformaInterceptor } from './interceptors/transform.interceptors';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 // Função assíncrona para inicializar a aplicação
@@ -20,15 +18,6 @@ async function bootstrap() {
 
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, document); 
-
- /*  // Define um prefixo global para todas as rotas da API
-  app.setGlobalPrefix('/api/v1');
-
-  // Aplica um pipe de validação global para todas as requisições
-  app.useGlobalPipes(new ValidationPipe());
-
-  // Aplica um interceptor global de transformação para todas as respostas
-  app.useGlobalInterceptors(new TransformaInterceptor()); */
 
   // Inicia a aplicação na porta 8080
   await app.listen(8080);
